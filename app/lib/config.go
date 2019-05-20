2c44a4af01e231e425bfffbde4ada7bda56a7e87
pottava <pottava@gmail.com>
2019-05-21 06:12:08 +0900

    display api version

--- app/lib/config.go
+++ app/lib/config.go
@@ -2,12 +2,22 @@
 package lib
 
 import (
+	"fmt"
+
 	"github.com/kelseyhightower/envconfig"
+)
+
+// for compile flags
+var (
+	version = "dev"
+	commit  string
+	date    = "---"
 )
 
 // Config can be set via environment variables
 type config struct {
-	AccessLog bool `envconfig:"ACCESS_LOG" default:"true"`
+	Version   string `envconfig:"VERSION" default:"dev"`
+	AccessLog bool   `envconfig:"ACCESS_LOG" default:"true"`
 }
 
 // Config represents its configurations
@@ -16,5 +26,8 @@ var Config *config
 func init() {
 	cfg := &config{}
 	envconfig.MustProcess("trivy", cfg)
+	if len(version) > 0 && len(commit) > 0 && len(date) > 0 {
+		cfg.Version = fmt.Sprintf("%s-%s (built at %s)", version, commit, date)
+	}
 	Config = cfg
 }
--- app/lib/http-handler-wrapper.go
+++ app/lib/http-handler-wrapper.go
@@ -1,6 +1,7 @@
 package lib
 
 import (
+	"fmt"
 	"log"
 	"net/http"
 	"time"
@@ -9,10 +10,23 @@ import (
 // Wrap wraps HTTP request handler
 func Wrap(handler http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		proc := time.Now()
-		handler.ServeHTTP(w, r)
-		if Config.AccessLog {
-			log.Printf("[%s] %.3f %s %s", r.RemoteAddr, time.Since(proc).Seconds(), r.Method, r.URL)
+		switch {
+		case eqauls(r, "/health"):
+			w.WriteHeader(http.StatusOK)
+
+		case eqauls(r, "/version"):
+			fmt.Fprintf(w, "%s", Config.Version)
+
+		default:
+			proc := time.Now()
+			handler.ServeHTTP(w, r)
+			if Config.AccessLog {
+				log.Printf("[%s] %.3f %s %s", r.RemoteAddr, time.Since(proc).Seconds(), r.Method, r.URL)
+			}
 		}
 	})
+}
+
+func eqauls(r *http.Request, url string) bool {
+	return url == r.URL.Path
 }
